f10d4876536db2a48d2239d2df3496a2699f0134
GabrielVictor <64982761+gabrielvictorcf@users.noreply.github.com>
2020-06-19 19:44:11 -0300

    Revert "Mineit (#3)"
    This reverts commit 6849b60e251b3fc24ca140be6811a663863d148a.

--- Desafio 4/js/index.js
+++ Desafio 4/js/index.js
@@ -11,10 +11,6 @@ function frame () {
 
 // This function will run every time the player presses a key
 document.body.addEventListener('keydown', event => {
-	if (event.key === 'D' || event.key === 'd' && player.hook.hookedObject instanceof Rock){
-		player.useDynamite();
-	};
-
 	// if that key is the spacebar, the player will try to throw it's hook.
 	if (event.key === ' ') player.throwHook();
 });
